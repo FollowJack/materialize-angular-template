fdaa248f7260a0d23f7522b231731fe16775fec3
Deniel Horvatic <deniel.horvatic@porschedigitallab.com>
2017-11-04 00:58:17 +0100

    Change to default style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import {Component, OnInit} from '@angular/core';
-import {toast} from "angular2-materialize";
+import {toast} from 'angular2-materialize';
+import {$} from 'protractor';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app works!';
+    title = 'app works!';
 
-  ngOnInit(): void {
-    toast("...and Materialize works as well!")
-  }
+    ngOnInit(): void {
+        // toast('...and Materialize works as well!');
+    }
 }
